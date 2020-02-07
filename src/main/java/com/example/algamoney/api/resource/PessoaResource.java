8ce6b089c69798b971d1d5aa39562c486c8eafa2
thiago.rodrigues <thiagomr58@gmail.com>
2020-02-07 11:51:52 -0300

    Implementando metodo para atualizar um registro de pessoa, integral e parcial ( somento atributo ativo );

--- src/main/java/com/example/algamoney/api/resource/PessoaResource.java
+++ src/main/java/com/example/algamoney/api/resource/PessoaResource.java
@@ -3,8 +3,11 @@ package com.example.algamoney.api.resource;
 import com.example.algamoney.api.event.RecursoCriadoEvent;
 import com.example.algamoney.api.model.Pessoa;
 import com.example.algamoney.api.repository.PessoaRepository;
+import com.example.algamoney.api.service.PessoaService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,6 +28,9 @@ public class PessoaResource {
     private PessoaRepository pessoaRepository;
 
     @Autowired
+    private PessoaService pessoaService;
+
+    @Autowired
     private ApplicationEventPublisher publisher;
 
     @GetMapping
@@ -33,7 +39,8 @@ public class PessoaResource {
     }
 
     @PostMapping
-    public ResponseEntity<Pessoa> cadastrar (@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
+    public ResponseEntity<Pessoa> cadastrar (@Valid @RequestBody Pessoa pessoa,
+                                             HttpServletResponse response) {
         Pessoa pessoaSalva = pessoaRepository.save(pessoa);
 
         publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
@@ -58,4 +65,20 @@ public class PessoaResource {
     public void remover(@PathVariable Long codigo) {
         pessoaRepository.deleteById(codigo);
     }
+
+    @PutMapping("/{codigo}")
+    public ResponseEntity<Pessoa> atualizar(@PathVariable Long codigo,
+                                            @Valid @RequestBody Pessoa pessoa){
+        Pessoa pessoaSalva = pessoaService.atualizar(codigo, pessoa);
+
+        return ResponseEntity.ok(pessoaSalva);
+
+    }
+
+    @PutMapping("/{codigo}/ativo")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizarProriedadeAtivo(@PathVariable Long codigo, @RequestBody Boolean ativo){
+        pessoaService.atualizarPropriedadeAtivo(codigo, ativo);
+    }
+
 }
